b692dabc9883fbc59b931227c079badc29034ca5
Jeremy Wells <jsheridanwells@gmail.com>
2020-05-15 13:53:11 -0500

    all main pages set up

--- assets/js/contact-validator.js
+++ assets/js/contact-validator.js
@@ -1,30 +1,28 @@
 'use strict';
-
-console.log('contact validating...');
-
-window.onload = function(){
+/**
+ * Add validation to contact form.
+ */
+window.onload = function() {
     const form = document.getElementById('contact-form');
-    const pristine = new Pristine(form);
-    const button = document.getElementById('contact-submit')
-        .addEventListener('click', e => {
-            e.preventDefault();
-            if (pristine.validate())
-            {
-                console.log('yer valid');
-            }
-            else
-            {
-                console.log('not ready yet');
-            }
-        });
+    const pristine = new Pristine(form, null, true);
+    const button = document.getElementById('contact-submit');
+    const inputs = document.querySelectorAll('.contact-input');
+    inputs.forEach(i => enableButtonIfValid(i, pristine, button));
+}
 
-    // form.addEventListener('keypress', e => {
-    //     e.preventDefault();
-    //     if (pristine.validate()){
-    //         console.log('too legit to quit');
-    //     }
-    //     else {
-    //         console.log('not ready yet');
-    //     }
-    // });
-};
+/**
+ * Adds listener to all inputs to enable submit button if
+ * all fields are valid.
+ * @param element
+ * @param pristine
+ * @param button
+ * @returns void
+ */
+function enableButtonIfValid(element, pristine, button) {
+    element.addEventListener('keydown', function(e){
+        if (pristine.validate())
+            button.disabled = false;
+        else
+            button.disabled = true;
+    });
+}
